9cf144867d41e4fea8928a8321b9fdd87f9a9246
Maksim Aisin <maksim.aisin@mail.ru>
2022-06-17 18:29:15 +0300

    feat: basic setup

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,11 +1,41 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from 'react'
+import data from './data'
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
+    const [count, setCount] = useState(0)
+    const [text, setText] = useState([])
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('it works')
+    }
+    return (
+        <section className='section-center'>
+            <h3>tired ob boring lorem ipsum?</h3>
+            <form className='lorem-form' onSubmit={handleSubmit}>
+                <label htmlFor='amount'>paragraphs:</label>
+                <input
+                    type='number'
+                    name='amount'
+                    id='amount'
+                    value={count}
+                    onChange={(e) => setCount(e.target.value)}
+                />
+                <button type='submit' className='btn'>
+                    generate
+                </button>
+            </form>
+            <article className='lorem-text'>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Error voluptatibus praesentium id aspernatur voluptatem,
+                    incidunt eos fugit excepturi alias modi.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Distinctio, tempora?
+                </p>
+            </article>
+        </section>
     )
 }
 
-export default App;
+export default App
